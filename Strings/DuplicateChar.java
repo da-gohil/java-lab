3808e5374216c79b36b285f8cc0357e913dde3b8
da-gohil <157419078+da-gohil@users.noreply.github.com>
2025-06-04 15:00:29 -0400

    Update DuplicateChar.java

--- Strings/DuplicateChar.java
+++ Strings/DuplicateChar.java
@@ -2,7 +2,6 @@
     WAJP that counts duplicate characters from a given string array
 */
 
-import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,18 +10,18 @@ public class DuplicateChar {
 
         String[] names = new String[] { "Danny", "Rohan", "Mohan" };
         for (String name : names) {
-            System.out.println(name);
+            // System.out.println(name);
             findDuplicateChar(name);
         }
     }
 
     public static void findDuplicateChar(String nameString) {
         Map<Character, Integer> count = new HashMap<>();
-
+        // Map ==> key, valoue pair for faster insertion and retrieval O(1)
         nameString = nameString.toLowerCase();
 
         for (char c : nameString.toCharArray()) {
-            System.out.println(c);
+            //System.out.println(c);
             if (Character.isLetter(c)) {
                 count.put(c, count.getOrDefault(c, 0) + 1);
             }
