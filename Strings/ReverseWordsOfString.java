ab57be8ddae69919560dbd352573c0a6d2eecdc3
da-gohil <157419078+da-gohil@users.noreply.github.com>
2025-06-08 15:11:55 -0400

    Create ReverseWordsOfString.java

---
+++ Strings/ReverseWordsOfString.java
@@ -0,0 +1,39 @@
+//WAJP to reverse the leters of the order not to reverse the individual characters
+// Eg: "Porsche is a sportscar, sportscar a is Porsche"
+import java.util.Scanner;
+import java.util.Stack;
+
+class ReverseWords{
+    public String reverseWords(String input){
+        String[] words = input.split("\\s+");
+
+        Stack<String> stack = new Stack<>();
+        for(String word: words){
+            stack.push(word);
+        }
+
+        StringBuilder reversed = new StringBuilder();
+        
+        while(!stack.isEmpty()){
+            reversed.append(stack.pop());
+            if(!stack.isEmpty()){
+                reversed.append(" ");
+            }
+        }
+        return reversed.toString();
+    }
+}
+
+public class ReverseWordsOfString{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+            System.out.println("Enter your String: ");
+            String userInput = scan.nextLine();
+            
+            ReverseWords rev = new ReverseWords();
+            String reversed = rev.reverseWords(userInput);
+
+            System.out.println(reversed);
+        scan.close();
+    }
+}
