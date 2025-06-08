28d6e3291c1a26e21d8fb799997f8e9934f83c1f
da-gohil <157419078+da-gohil@users.noreply.github.com>
2025-06-08 16:43:29 -0400

    Update ReverseWordsOfString.java

--- Strings/ReverseWordsOfString.java
+++ Strings/ReverseWordsOfString.java
@@ -37,3 +37,17 @@ public class ReverseWordsOfString{
         scan.close();
     }
 }
+
+// Leetcode Problem: 151
+class Solution {
+    public String reverseWords(String s) {
+    
+        String [] words = s.split(" +");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length -1; i>=0; i--){
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
